4cde83267b732abf1da9b761fae2ad4f1c194b3b
Freda.Chen <git config --global user.name Bonny Chen>
2014-11-21 22:44:09 +0800

    fix url

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ var loadPagesInfo = function(pages){
 
       FB.api(item.id + '/picture?type=large', function(response){
         // 塞資料到 html 中
-        $page.find('.thumbnail img').attr('src', response.data.rul);
+        $page.find('.thumbnail img').attr('src', response.data.url);
         $page.appendTo(current);
         counter++;
         // 塞完資料以後處理一下斷行
