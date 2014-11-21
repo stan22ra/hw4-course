53698a1c7cd91270d3ae92966d04a20ce280612c
Freda.Chen <git config --global user.name Bonny Chen>
2014-11-21 22:42:23 +0800

    add insert

--- js/app.js
+++ js/app.js
@@ -75,6 +75,7 @@ var loadPagesInfo = function(pages){
       FB.api(item.id + '/picture?type=large', function(response){
         // 塞資料到 html 中
         $page.find('.thumbnail img').attr('src', response.data.rul);
+        $page.appendTo(current);
         counter++;
         // 塞完資料以後處理一下斷行
         if(counter===pages.length){
